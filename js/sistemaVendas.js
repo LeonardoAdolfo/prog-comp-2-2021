261c443727a004d2eebb4fdb3f96b1fedc908c1e
Leonardo Adolfo <adolfoleo625@gmail.com>
2021-08-31 22:07:36 -0300

    31-08

--- js/sistemaVendas.js
+++ js/sistemaVendas.js
@@ -3,7 +3,7 @@ let sistemaVendas = ()=>{
     let vetVendedores = []
     let vetVendas = []
    do{
-     opcao = Number(prompt((`Informe:\n1. Cadastrar Vendedor \n 2. Cadastrar Vendas \n 3. Sair`))) 
+     opcao = Number(prompt((`Informe:\n1. Cadastrar Vendedor \n 2. Cadastrar Vendas \n 3. Ver valor de venda \n 4. Sair`))) 
     switch(opcao){
         case 1:
             let objeto= {
@@ -42,10 +42,41 @@ let sistemaVendas = ()=>{
             }
             console.log(vetVendas)
             break
-        case 3: alert(`O Programa sera encerrado !!!`)
+        case 3: let codigo = Number(prompt(`Informe o codigo do vendedor !!`))
+        case 3: let mes = Number(prompt(`Informe o mes da venda !!`))
+        let achou3 = false 
+                 for (let i = 0; i < vetVendas.length; i++) {
+                     if ((vetVendas[i].codigo == codigo) && (vetVendas[i].mes == mes)) {
+                         console.log(`O valor de vendas do funcionario ${codigo} do mes ${mes} foi ${vetVendas[i].valor}`)
+                         achou3= true
+                     }
+                 }
+                 if(!achou3){
+                     console.log(`NÃO ACHOU A VENDA`)
+                 }
+            break
+        case 4: alert(`Programa encerrado`)
+            let codigo4 = Number(prompt(`Informe o codigo do vendedor`))
+            let soma = 0
+            let achou4 = false 
+            for (let i = 0; i < vetVendas.length; i++) {
+                if (vetVendas[i].codigo == codigo4) {
+                    soma += vetVendas[i].valor
+                    achou4 =  true 
+                }
+            }
+            if (!achou4) {
+                console.log(`Venda não encontrada para este funcionario`)
+            }
+            else{
+                console.log(`O total de vendas do vendedor ${codigo4} foi ${soma}`)
+            }
+
+            break
+            case 5: alert(`Programa encerrado`)
             break
         default: alert(`Opção Invalida !!`)
     }
    }
-   while(opcao!=3)
+   while(opcao!=4)
 }
