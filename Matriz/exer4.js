07f0d0d179f7f1b07d9b5ba6fe9e791cf2f30767
Leonardo Adolfo <adolfoleo625@gmail.com>
2021-10-06 20:33:35 -0300

    03-10

--- Matriz/exer4.js
+++ Matriz/exer4.js
@@ -6,6 +6,31 @@ let sistemaVendas =()=>{
     //Declara Matriz
     let matriz = []
 
+    let opcao
+    
+    do{
+        opcao = Number(prompt(`1. Cadastrar Vendas \n2. Total Vendido por mes \n3.Venda semana \n4. Vendas ano \n5.Sair `))
+        switch(opcao){
+        case 1: cadastraVEndas(matriz, meses)
+        break
+        case 2: totalVendas(matriz, meses)
+        break
+        case 3: totalVendaSemana(matriz)
+        break
+        case 4: totalVendasAno(matriz)
+        break
+        case 5: alert('Saiu ne mano')
+        break
+        default: alert('Opção Invalida')
+        break
+    }
+    }while(opcao != 5)
+
+
+    
+}
+
+let cadastraVEndas = (matriz, meses) => {
     for(let i = 0; i<12;i++){
         matriz[i] = []
         alert(`Informe das vendas do mes ${meses[i]}`)
@@ -14,6 +39,10 @@ let sistemaVendas =()=>{
             
         }
     }
+}
+
+let totalVendas = (matriz, meses) => {
+    
     let soma 
     for(let i = 0; i<12;i++){
        soma =0
@@ -22,5 +51,27 @@ let sistemaVendas =()=>{
             soma = soma + matriz[i][j]
         }
         alert(`Total vendido no mes ${i} foi ${soma}`)
+    }
+}
+
+let totalVendaSemana=(matriz)=>{
+    let soma = 0
+    for (let j = 0;j  < 4; j++) {
+        for (let i = 0; i < 12; i++) {
+            soma = soma + matriz[i][j]
+        }
+        alert(`A soma de vendas da semana ${j+1} é ${soma} `)
+        
+    }
+}
+
+let totalVendasAno = (matriz) =>{
+    let soma = 0
+    for (let j = 0;j  < 12; j++) {
+        for (let i = 0; i <12 ; i++) {
+            soma = soma + matriz[j][i]
+        }
+        alert(`A soma de vendas do ano é ${soma} `)
+        
     }
 }
